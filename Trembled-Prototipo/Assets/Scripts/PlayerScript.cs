8b98dc514df290e5f2327cb24bf706416a882b12
NahuelALB <ivanalbarado8@gmail.com>
2025-11-24 18:29:43 -0300

    Importar y animar personaje de Trembled
    Se logró realizar todo lo del personaje con exito

--- Trembled-Prototipo/Assets/Scripts/PlayerScript.cs
+++ Trembled-Prototipo/Assets/Scripts/PlayerScript.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
@@ -7,6 +9,8 @@ public class PlayerScript : MonoBehaviour
 
     public float speed = 4;
     public float rotationSpeed = 10;
+
+    public Animator anim_controller;
 
     private Vector3 forward, right;
 
@@ -20,7 +24,7 @@ public class PlayerScript : MonoBehaviour
         right = Camera.main.transform.right;
         right.y = 0;
         right = Vector3.Normalize(right);
-        
+
     }
 
     // Update is called once per frame
@@ -29,6 +33,8 @@ public class PlayerScript : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
+        
+
         Vector3 direction = horizontalInput * right + verticalInput * forward;
         if (direction.magnitude > 0.1f)
         {
@@ -36,7 +42,13 @@ public class PlayerScript : MonoBehaviour
 
             Quaternion targetRotation = Quaternion.LookRotation(direction);
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed*Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            anim_controller.SetFloat("move", direction.magnitude);
+        }
+        else
+        {
+            anim_controller.SetFloat("move", 0f);
         }
     }
 }
